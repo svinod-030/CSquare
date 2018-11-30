5f1811ff972f275d5eebc3bdcefe7b8662ae04d2
unknown <vinodsigadana030@gmail.com>
2018-11-30 10:09:35 +0530

    Just for testing

--- BootPCFTest/src/main/java/com/csquare/util/Constants.java
+++ BootPCFTest/src/main/java/com/csquare/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 	public static String login_success="login.success";
 	public static String login_failure="login.failure.password.mismatch";
 	public static String login_email_not_exists="login.email.not.exists";
+	public static String login_email_not_exists1="login.email.not.exists";
 }
